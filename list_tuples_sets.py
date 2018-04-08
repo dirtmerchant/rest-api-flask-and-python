0f6d3fb908c6ba74be376241676655179d7dac5e
Bert Bullough <bert@bertbullough.com>
2018-04-08 17:02:54 -0600

    section 2.13

--- list_tuples_sets.py
+++ list_tuples_sets.py
@@ -1,7 +1,19 @@
 my_variable = "hello"
 
-grade_one = 77
-grade_two = 80
-grade_three = 90
+#grade_one = 77
+#grade_two = 80
+#grade_three = 90
 
-print((grade_one + grade_two + grade_three) / 3)
+#print((grade_one + grade_two + grade_three) / 3)
+
+#list
+grades = [77, 80, 90, 95, 100]
+#tuple immutable
+tuple_grades = (77, 80, 90, 95, 100)
+#set unique and unordered
+set_grades = {77, 80, 90, 100, 100}
+
+print(sum(grades) / len(grades))
+grades.append(108)
+
+print(set_grades)
