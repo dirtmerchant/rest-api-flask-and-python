329ab5d1af77907b9369c7af4609a89488625fb6
Bert Bullough <bert@bertbullough.com>
2018-04-08 10:45:16 -0600

    method exercise

---
+++ methods.py
@@ -0,0 +1,23 @@
+## Complete the method by making sure it returns 42. .
+#def return_42(result):
+#    # Complete method here
+#    pass  # 'pass' just means "do nothing". Make sure to delete this!
+#    print(result)
+#
+## Create a method below, called my_method, that takes two arguments and returns the result of its two arguments multiplied together.
+#
+#def the_answer(number_one, number_two):
+#    return number_one * number_two
+#
+#result = the_answer(6, 7)
+#print(result)
+
+
+# Complete the method by making sure it returns 42. .
+def return_42():
+    # Complete method here
+    return 42  # 'pass' just means "do nothing". Make sure to delete this!
+
+# Create a method below, called my_method, that takes two arguments and returns the result of its two arguments multiplied together.
+def my_method(arg1, arg2):
+   return arg1 * arg2 
