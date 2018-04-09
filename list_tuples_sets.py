12dcaff787bbdf9a37a2bc2289a7dd6a5148d13b
Bert Bullough <bert@bertbullough.com>
2018-04-08 20:25:07 -0600

    intersection, union, & difference

--- list_tuples_sets.py
+++ list_tuples_sets.py
@@ -22,5 +22,9 @@ set_grades.add(60)
 your_lottery_numbers = {1, 2, 3, 4, 5}
 winning_numbers = {1, 3, 5, 7, 9, 11}
 
+#intersection
 #print(your_lottery_numbers.intersection(winning_numbers))
-print(your_lottery_numbers.union(winning_numbers))
+#union
+#print(your_lottery_numbers.union(winning_numbers))
+#difference
+#print({1, 2, 3, 4}.difference({1, 2}))
