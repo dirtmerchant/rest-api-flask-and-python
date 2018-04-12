02139a97345cc0941ec9b80a2a30717d11163a2c
Bert Bullough <bert@bertbullough.com>
2018-04-11 19:43:06 -0600

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,35 @@
+my_set = {1, 3, 5}
+my_dict = {'name': 'Jose', 'age': 90}
+another_dict = {1: 15, 2: 75, 3: 150}
+
+lottery_player = {
+    'name': 'Rolf',
+    'numbers': (13, 45, 66, 23, 22)
+}
+
+lottery_player = {
+    'name': 'Rolf',
+    'numbers': (13, 45, 66, 23, 22)
+}
+
+lottery_player = {
+    'name': 'John',
+    'numbers': (14, 56, 80, 23, 22)
+}
+
+universities = [
+    {
+        'name': 'Oxford',
+        'location': 'UK'
+    },
+    {
+        'name': 'MIT',
+        'location': 'US'
+    }
+]
+
+# sum(lottery_player[0]'numbers'])
+
+lottery_player[0].total()
+
+lottery_player.total()
