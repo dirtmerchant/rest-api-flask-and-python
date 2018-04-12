c78f7a855005ef9fcb8ddd29bf4044801f8b2b05
Bert Bullough <bert@bertbullough.com>
2018-04-11 18:59:50 -0600

    list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,9 @@
+my_list = [0, 1, 2, 3, 4]
+an_equal_list = [x for x in range(5)]
+
+multiply_list = [x * x for x in range(5)]
+# print(multiply_list)
+
+print(8 % 3)
+
+print([n for n in range(10) if n % 2 == 0])
