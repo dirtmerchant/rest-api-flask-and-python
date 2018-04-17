ab58b6ea5e5b35c949996275d4e44aad5eaac59a
Bert Bullough <bert@bertbullough.com>
2018-04-16 20:06:07 -0600

    classes objects

---
+++ classes_objects.py
@@ -0,0 +1,17 @@
+lottery_player_dict = {
+    'name': 'Rolf',
+    'numbers': (5, 9, 12, 3, 1, 21)
+}
+
+class LotteryPlayer:
+    def __init__(self, name):
+        self.name = name
+        self.numbers = (5, 9, 12, 3, 1, 21)
+
+    def total(self):
+        return sum(self.numbers)
+
+player_one = LotteryPlayer("Rolf")
+player_two = LotteryPlayer("John")
+
+print(player_one.name == player_two.name)
